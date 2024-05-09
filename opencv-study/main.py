147b9538aaca21eaddb95263c59c9f327d7869cb
유상기 <lapetus@sktai.io>
2024-05-09 16:28:25 +0900

    테스트

--- opencv-study/main.py
+++ opencv-study/main.py
@@ -5,8 +5,14 @@ image = cv2.imread("files/yumtory.png")
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+cv2.circle(gray_image, (300, 100), 60, (255, 0, 0), 3, cv2.LINE_AA)
+cv2.circle(gray_image, (300, 100), 30, (255, 255, 0), -1, cv2.LINE_AA)
+
 # 결과 이미지 출력
 cv2.imshow("Original Image", image)
+cv2.waitKey(0)
+
 cv2.imshow("Gray Image", gray_image)
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
