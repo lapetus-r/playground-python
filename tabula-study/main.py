cdfe524a94c29bb070b8a7b5a0b4450ceb58744d
유상기 <lapetus@sktai.io>
2024-05-15 11:46:00 +0900

    feat: 수업 내용 반영

--- tabula-study/main.py
+++ tabula-study/main.py
@@ -1,13 +1,15 @@
+import pandas as pd
 import tabula
 
 pdf_path = "https://github.com/chezou/tabula-py/raw/master/tests/resources/data.pdf"
 
-dfs = tabula.read_pdf("files/datasheet-cc-8800-1_removed.pdf", stream=True, pages="all")
-# read_pdf returns list of DataFrames
-print(len(dfs))
-print(len(dfs[0]))
+df = tabula.read_pdf("files/datasheet-cc-8800-1_removed.pdf", stream=True, pages="all")[0]
+df.rename(columns={'(m)': 'boom_length', '(m).1': "super_lift_ballast"}, inplace=True)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
-for index, table in enumerate(dfs):
-    print(f"\nData Index: {index}")
-    print(type(table))
-    print(table)
+df = df.loc[df['super_lift_ballast'] == 0]
+print(df)
+
+results = df.loc[df['boom_length'] == 108.0, ['16']]
+print(results)
