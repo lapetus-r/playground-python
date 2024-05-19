68cb5746120e19e8253ed1ace5d7b6ad7367df8b
유상기 <lapetus@sktai.io>
2024-05-19 09:04:26 +0900

    refactor: Formatting

--- tabula-study/main.py
+++ tabula-study/main.py
@@ -3,13 +3,15 @@ import tabula
 
 pdf_path = "https://github.com/chezou/tabula-py/raw/master/tests/resources/data.pdf"
 
-df = tabula.read_pdf("files/datasheet-cc-8800-1_removed.pdf", stream=True, pages="all")[0]
-df.rename(columns={'(m)': 'boom_length', '(m).1': "super_lift_ballast"}, inplace=True)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+df = tabula.read_pdf("files/datasheet-cc-8800-1_removed.pdf", stream=True, pages="all")[
+    0
+]
+df.rename(columns={"(m)": "boom_length", "(m).1": "super_lift_ballast"}, inplace=True)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
 
-df = df.loc[df['super_lift_ballast'] == 0]
+df = df.loc[df["super_lift_ballast"] == 0]
 print(df)
 
-results = df.loc[df['boom_length'] == 108.0, ['16']]
+results = df.loc[df["boom_length"] == 108.0, ["16"]]
 print(results)
